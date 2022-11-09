6ee7b5fd48edd5fe36986af4e3ed7378566ce83a
Sus <oliversharp2005@gmail.com>
2022-11-09 06:57:05 +0000

    a

---
+++ index.js
@@ -0,0 +1,21 @@
+var video = document.getElementById('video');
+var source = document.createElement('source');
+
+source.setAttribute('src', 'Jesus.mp4');
+let num;
+if (num == 1) {
+    source.setAttribute('src', 'Jesus.mp4');
+} else if (num == 2) {
+    source.setAttribute('src', 'Jesus2.mp4');
+}
+
+source.setAttribute('type', 'video/mp4');
+video.autoplay = true;
+video.appendChild(source);
+
+console.log({
+  src: source.getAttribute('src'),
+  type: source.getAttribute('type'),
+});
+
+//document.body.style.backgroundColour = '#fff'
